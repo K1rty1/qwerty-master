64d1d2fb892c26a89a0d2c1fd3c2dd1ec888324a
vova <alo>
2022-11-29 13:29:50 +0500

    Добавьте файлы проекта.

---
+++ qwerty/Model/Core.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qwerty.Model
+{
+    public partial class Core
+    {
+        public TiresCompanyEntities context = new TiresCompanyEntities();
+        public string ImagePath { get {
+                if (Image==null)
+                {
+
+                }
+                else
+                {
+                return ""+Image;
+                }
+
+            } }
+    }
+}
